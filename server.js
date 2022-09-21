eb8a5a7c1b388fc4f6cec53804ae267e9dfa65a2
sachith <sachithprabhashana@gmail.com>
2022-09-21 23:43:27 +0530

    json file changes

--- server.js
+++ server.js
@@ -7,5 +7,16 @@ app.use(express.json())
 app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
 app.use('/api/bookings/' , require('./routes/bookingsRoute'))
+
+const path =require('path')
+if(process.env.NODE_ENV==='production'){
+    app.use('/', express.static('client/build'))
+    app.use('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client/build/index.html'));
+    })
+}
+
 app.get('/',(req, res) => res.send('hello world!'));
+
+
 app.listen(port, () => console.log(`node js server started in port ${port}`));
